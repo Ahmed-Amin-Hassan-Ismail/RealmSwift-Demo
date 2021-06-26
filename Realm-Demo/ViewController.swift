b1d96bbe85336c0e0b6418cc0567bcfcc47f1b3d
Ahmed Amin <Ahmedaminhassanismail@gmail.com>
2021-06-26 16:37:39 +0200

    initial commit

---
+++ Realm-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm-Demo
+//
+//  Created by Amin  on 6/26/21.
+//  Copyright © 2021 AhmedAmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
